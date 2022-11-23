b5634cf7f1f20d1b35d55d32ee0bf42a4521841b
Manuel Jesús López <51477144+manulopezayora@users.noreply.github.com>
2022-11-23 20:08:20 +0100

    [DEV] Add pagination component

--- src/app/rick-and-morty/components/pagination/pagination.component.ts
+++ src/app/rick-and-morty/components/pagination/pagination.component.ts
@@ -1,8 +1,6 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
-import { take } from 'rxjs';
-import { RickMortyService } from '../../services/rick-morty.service';
-import { Character, CharacterInfo } from '../../model/character.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Character } from '../../model/character.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
@@ -11,41 +9,37 @@ import { Character, CharacterInfo } from '../../model/character.model';
 })
 export class PaginationComponent implements OnInit {
 
-  @Input() characterData: Character;
+  @Input() characters: Character;
+  @Input() currentPage: number;
+  @Output() sendCurrentPage = new EventEmitter<number>();
 
-  public paginationNumbers!: number[];
-
-  constructor(
-    private rickMortyService: RickMortyService
-  ) {
-    this.characterData = {};
-   }
+  constructor() {}
 
   ngOnInit(): void {
-    this.paginationNumbers = this.setPaginationNumbers(this.characterData);
-    console.log(this.paginationNumbers)
+    this.currentPage = this.currentPage ?? 1;
   }
 
-  public getCharactersByPage(page: number) {
-    this.rickMortyService.getCharactersByPage(page).pipe(take(1)).subscribe(
-      {
-        next: (characters: Character) => {
-          console.log(characters)
-        },
-        error: (error: HttpErrorResponse) => {
-          console.log(error.status)
-        }
-      }
-    )
-  }
-
-  private setPaginationNumbers(pagesIterator: Character){
-    let totalNumberArray: number[] = [];
-    const counter = pagesIterator.info?.pages || 0;
-    for (let i = 0; i < counter; i++) {
-      totalNumberArray.push(i + 1);
+  public previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.sendCurrentPage.emit(this.currentPage);
     }
-    return totalNumberArray;
   }
+
+  public nextPage(): void {
+    if (this.currentPage >= 1) {
+      this.currentPage++;
+      this.sendCurrentPage.emit(this.currentPage);
+    }
+  }
+
+  // private setPaginationNumbers(pagesIterator: Character){
+  //   let totalNumberArray: number[] = [];
+  //   const counter = pagesIterator.info?.pages || 0;
+  //   for (let i = 0; i < counter; i++) {
+  //     totalNumberArray.push(i + 1);
+  //   }
+  //   return totalNumberArray;
+  // }
 
 }
